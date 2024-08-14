165e230c86bec1483f80495f11ba0be069844587
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-13 21:48:52 -0300

    Creação do User Service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.tdo';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [];
+
+  async createUser(CreateUserDto: CreateUserDto): Promise<User> {
+    return {
+      ...CreateUserDto,
+      id: 1,
+    };
+  }
+}
