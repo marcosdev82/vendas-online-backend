988e71570907d680fa98280d6b81bf7e8d24a0f0
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-13 22:07:43 -0300

    Usando o bcrypt na senha

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.tdo';
 import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
   private users: User[] = [];
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+
+    const passwordHash = await hash(createUserDto.password, saltOrRounds);
+
+    console.log('passwordHashed', passwordHash);
+
     return {
       ...createUserDto,
       id: 1,
