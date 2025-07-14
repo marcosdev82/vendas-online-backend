1658c87c61515fce7d0081725d8b2123dea04ee8
mikek182 <marcosvinicius@geekwork.com.br>
2025-07-14 20:02:12 -0300

    bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { User } from './intreface/user.interface';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
 
     private users: User[] = [];
-    async createUser(createUser: CreateUserDto): Promise<User> {
-        return {
-            ...createUser,
-            id: 1,
+    async createUser(CreateUserDto: CreateUserDto): Promise<User> {
+
+        const salOrRounds = 10;
+
+        const passwordHashed = await hash(CreateUserDto.password, salOrRounds)
+
+        const user: User = {
+            ...CreateUserDto,
+            id: this.users.length + 1,
+            password: passwordHashed,
         }
+
+        this.users.push(user);
+        return user;
     };
 
 }
