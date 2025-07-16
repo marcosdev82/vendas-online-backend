9a30b73e12ce226ac91b6fbd89df73d65cce54b7
mikek182 <marcosvinicius@geekwork.com.br>
2025-07-16 13:20:57 -0300

    user interface decorator

--- src/user/intreface/user.interface.ts
+++ src/user/intreface/user.interface.ts
@@ -1,8 +1,23 @@
-export interface User {
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'user'})
+export class User {
+
+    @PrimaryGeneratedColumn('rowid')
     id: number;
+
+    @Column({name: 'name', nullable: false})
     name: string;
+    
+    @Column({name: 'email', nullable: false})
     email: string;
+
+    @Column({name: 'phone', nullable: false})
     phone: string;
+
+    @Column({name: 'cpf'})
     cpf: string;
+
+    @Column({name: 'password', nullable: false})
     password?: string;
 }
